55dcb59c659b4137c3111e9ef9a013c2265ccd65
Michele Shayane <106595533+michele09082007@users.noreply.github.com>
2023-10-27 16:13:24 -0300

    ;,

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function TocaSomPom(){
-document.querySelector('#som_tecla_pom').play();
+function TocaSomPom() {
+    document.querySelector('#som_tecla_pom').play();
 }
-document.querySelector('.tecla_pom').onclick =TocaSomPom;
+document.querySelector('.tecla_pom').onclick = TocaSomPom;
